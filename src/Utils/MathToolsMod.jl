9bd5bce9556021bf74ade3de03fbced46be3a0e5
cfranken <fronge@gmail.com>
2019-04-30 14:47:59 -0700

    Updates

---
+++ src/Utils/MathToolsMod.jl
@@ -0,0 +1,63 @@
+module MathToolsMod
+
+#-----------------------------------------------------------------------
+# !DESCRIPTION:
+# Math tools
+#
+# !USES:
+#use shr_kind_mod, only : r8 => shr_kind_r8
+#use clm_varctl, only : iulog
+#use abortutils, only : endrun
+#use CanopyFluxesMultilayerType, only : mlcanopy_type
+#!
+#! !PUBLIC TYPES:
+#implicit none
+#!
+#! !PUBLIC MEMBER FUNCTIONS:
+export quadratic,beta_function,log_gamma_function
+# hybrid             ! Solve for the root of a function using secant and Brent's methods
+# zbrent             ! Use Brent's method to find the root of a function
+# quadratic          ! Solve a quadratic equation for its two roots
+# tridiag            ! Solve a tridiagonal system of equations
+# beta_function      ! Evaluate the beta function at p and q: B(p,q)
+# log_gamma_function ! Evaluate the log natural of the gamma function at x: ln(G(x))
+
+#-----------------------------------------------------------------------
+function quadratic(a, b, c)
+  discr = b^2 - 4*a*c
+  discr >= 0 ?   ( (-b + sqrt(discr))/(2a), (-b - sqrt(discr))/(2a) ) : error("Only complex roots")
+end # function
+
+
+#-----------------------------------------------------------------------
+function beta_function(p, q)
+#
+# !DESCRIPTION:
+# Return the value of the beta function evaluated at p and q: B(p,q)
+#
+# !USES:
+#
+exp(log_gamma_function(p) + log_gamma_function(q) - log_gamma_function(p+q))
+
+end #function beta_function
+
+#-----------------------------------------------------------------------
+function log_gamma_function(x)
+#
+# !DESCRIPTION:
+# Return the value of the log natural of the gamma function evaluated at x: ln(G(x))
+#
+  coef = [76.18009172947146, -86.50532032941677,24.01409824083091, -1.231739572450155, 0.1208650973866179e-02, -0.5395239384953e-05];
+  stp = 2.5066282746310005;
+  y = x
+  tmp = x + 5.5
+  tmp = (x + 0.5) * log(tmp) - tmp
+  ser = 1.000000000190015;
+  for co in coef
+    y = y + 1.0
+    ser = ser + co / y
+  end
+  return tmp + log(stp*ser/x)
+end # function log_gamma_function
+
+end # module MathToolsMod
