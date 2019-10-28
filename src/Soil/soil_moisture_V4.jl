6d746dbd8674606f36019d98bc1df557f1ce79bf
Pierre Gentine <pg2328@columbia.edu>
2019-10-28 17:25:16 -0400

    Baby steps
    Starting to update the soil msoiture component to integrate the soil heat diffusion equation

--- src/Soil/soil_moisture_V4.jl
+++ src/Soil/soil_moisture_V4.jl
@@ -36,68 +36,80 @@ export soil_struct, van_Genuchten, Campbell, tridiagonal_solver, matric_potentia
 end
 
 
+
+
+
+
+
+
+
+
 # Van Genuchten Function
 function van_Genuchten(params, psi)
 
-# ----------------------------------
-# van Genuchten [1980] relationships
-# ----------------------------------
+    # ----------------------------------
+    # van Genuchten [1980] relationships
+    # ----------------------------------
 
-# --- Soil parameters
+    # --- Soil parameters
 
-theta_res = params[1];   # Residual water content
-theta_sat = params[2];   # Volumetric water content at saturation
-alpha = params[3];       # Inverse of the air entry potential
-n = params[4];           # Pore-size distribution index
-m = params[5];           # Exponent
-Ksat = params[6];        # Hydraulic conductivity at saturation
-ityp = params[7];        # Soil texture flag
+    theta_res = params[1];   # Residual water content
+    theta_sat = params[2];   # Volumetric water content at saturation
+    alpha = params[3];       # Inverse of the air entry potential
+    n = params[4];           # Pore-size distribution index
+    #m = params[5];           # Exponent
+    Ksat = params[6];        # Hydraulic conductivity at saturation
+    ityp = params[7];        # Soil texture flag
 
-# --- Effective saturation [Se] for specified matric potential [psi]
+    # added Gentine, m is known as a function of n
+    m = 1-1/n
 
-if (psi <= 0)
-   Se = (1 + (alpha * abs(psi))^n)^-m
-else()
-   Se = 1
-end
+    # --- Relative saturation [Se] for specified matric potential [psi]
+    if (psi < 0)
+       Se = (1 + (alpha * abs(psi))^n)^-m
+    else()
+       Se = 1
+    end
 
-# --- Volumetric soil moisture [theta] for specified matric potential [psi]
+    # --- Volumetric soil moisture [theta] for specified matric potential [psi]
+    theta = theta_res + (theta_sat - theta_res) * Se
 
-theta = theta_res + (theta_sat - theta_res) * Se
+    # --- Hydraulic conductivity [K] for specified matric potential [psi]
+    if (Se < 1)
+       K = Ksat * sqrt(Se) * (1 - (1 - Se^(1/m))^m)^2
+       # Special case for Haverkamp et al. (1977) sand [ityp = 1] & Yolo light clay [ityp = 2]
+       if (ityp == 1)
+          K = Ksat * 1.175e6 / (1.175e6 + abs(psi)^4.74)
+       end
+       if (ityp == 2)
+          K = Ksat * 124.6/ (124.6 + abs(psi)^1.77)
+       end
+    else()
+       K = Ksat
+    end
 
-# --- Hydraulic conductivity [K] for specified matric potential [psi]
+    # --- Specific moisture capacity [cap] for specified matric potential [psi]
+    if (psi < 0)
+        num = alpha * m * n * (theta_sat - theta_res) * (alpha * abs(psi))^(n-1)
+        den =  (1 + (alpha * abs(psi))^n)^(m+1)
+        cap = num / den
+    else()
+        cap = 0
+    end
 
-if (Se <= 1)
-   K = Ksat * sqrt(Se) * (1 - (1 - Se^(1/m))^m)^2
+    # --- dtheta_dpsi required to copmute diffusivity of head based tepomral Richards' equation
+    if (psi < 0)
+        dtheta_dpsi = (theta_sat - theta_res) * m*n*alpha^n*abs(psi)^(n-1)*(1 + (alpha * abs(psi))^n)^(-m-1)
+    else()
+        dtheta_dpsi = 0
+    end
 
-   # Special case for Haverkamp et al. (1977) sand [ityp = 1] & Yolo light clay [ityp = 2]
-
-   if (ityp == 1)
-      K = Ksat * 1.175e6 / (1.175e6 + abs(psi)^4.74)
-   end
-   if (ityp == 2)
-      K = Ksat * 124.6/ (124.6 + abs(psi)^1.77)
-   end
-
-else()
-
-   K = Ksat
-
-end
-
-# --- Specific moisture capacity [cap] for specified matric potential [psi]
-
-if (psi <= 0)
-   num = alpha * m * n * (theta_sat - theta_res) * (alpha * abs(psi))^(n-1)
-   den =  (1 + (alpha * abs(psi))^n)^(m+1)
-   cap = num / den
-else()
-   cap = 0
-end
-
-return theta, K, cap
+    return theta, K, cap, dtheta_dpsi
 
 end # function
+
+
+
 
 function Campbell(params, psi)
 
@@ -139,6 +151,14 @@ end
 return theta, K, cap
  
 end # Function
+
+
+
+
+
+
+
+
 
 # Other Functions
 function tridiagonal_solver(a, b, c, d, n)
@@ -194,6 +214,14 @@ end
 return u
 
 end # function
+
+
+
+
+
+
+
+
 
 function matric_potential(type, params, theta)
 
