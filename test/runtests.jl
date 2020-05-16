837bc563a8ca66aab0c5e1178f928447b828aa26
Christian Frankenberg <fronge@gmail.com>
2020-05-16 16:32:20 -0700

    separated experiments for tests to go without plots

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,5 @@ ENV["JULIA_LOG_LEVEL"] = "WARN"
 
 @testset "Land" begin
     include(joinpath(@__DIR__, "..", "experiments", "DiurnalCycle.jl"))
-    include(joinpath(@__DIR__, "..", "notebooks", "Radiation_Test_BRDF.jl"))
+    include(joinpath(@__DIR__, "..", "experiments", "Radiation_Test_BRDF.jl"))
 end
