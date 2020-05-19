24372e3f88673877ee15d802dcd3f32e213756aa
Christian Frankenberg <fronge@gmail.com>
2020-05-19 14:48:01 -0700

    2nd try

--- notebooks/Radiation_Test_BRDF.jl
+++ notebooks/Radiation_Test_BRDF.jl
@@ -5,6 +5,9 @@
 
 ## Use Julia Plots package and switch to plotly js option:
 using Plots
+using Conda
+Conda.add("matplotlib")
+
 pyplot()
 #plotlyjs()
 #----------------------------------------------------------------------------
