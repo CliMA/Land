b1b8388aee38a7e6dbcc56e9a4f336430b83cc0a
Yujie Wang <jesiner@gmail.com>
2020-07-06 11:24:55 -0700

    add stomatal models

---
+++ src/refresh.jl
@@ -0,0 +1,242 @@
+###############################################################################
+#
+# Update Leaves struct when temperature changes
+#
+###############################################################################
+"""
+    update_leaf!(
+            photo_set::AbstractPhotoModelParaSet{FT},
+            leaves::Leaves{FT},
+            envir::AirLayer{FT})
+
+Update leaf physiological parameters if temperature or pressure changes in the
+daytime, given
+- `photo_set` [`C3ParaSet`](@ref) or [`C4ParaSet`](@ref) type parameter set
+- `leaves` [`Leaves`](@ref) type struct
+- `envir` [`AirLayer`](@ref) type struct
+"""
+function update_leaf_TP!(
+            photo_set::AbstractPhotoModelParaSet{FT},
+            leaves::Leaves{FT},
+            envir::AirLayer{FT}
+            ) where {FT<:AbstractFloat}
+    # unpack required variables
+    @unpack g_max25, g_min25, p_ups, p_old, T, T_old = leaves;
+
+    # if T changes, update TD and ec, then T_old
+    if T != T_old
+        leaves.g_max    = g_max25 * relative_diffusive_coefficient(T);
+        leaves.g_min    = g_min25 * relative_diffusive_coefficient(T);
+        leaves.LV       = latent_heat_vapor(T) * 1000 / FT(MOLMASS_WATER);
+        leaves.ps.T     = leaves.T;
+        leaf_temperature_dependence!(photo_set, leaves.ps, envir);
+        leaves.p_sat    = leaves.ps.p_sat;
+        leaves.hs.f_st  = relative_surface_tension(T);
+        leaves.hs.f_vis = relative_viscosity(T);
+        leaves.hs.p_ups = leaves.p_ups;
+        leaves.ec       = leaf_e_crit(leaves.hs, leaves.ec);
+        leaves.T_old    = leaves.T;
+        leaves.p_old    = leaves.p_ups;
+    # if only p_ups changes, update ec and p_old
+    elseif p_ups != p_old
+        leaves.hs.p_ups = leaves.p_ups;
+        leaves.ec       = leaf_e_crit(leaves.hs, leaves.ec);
+        leaves.p_old    = leaves.p_ups;
+    end
+
+    return nothing
+end
+
+
+
+
+"""
+    update_leaf_AK!(
+            photo_set::AbstractPhotoModelParaSet{FT},
+            leaves::Leaves{FT},
+            envir::AirLayer{FT})
+
+Update leaf maximal A and K for Sperry model, given
+- `photo_set` [`C3ParaSet`](@ref) or [`C4ParaSet`](@ref) type parameter set
+- `leaves` [`Leaves`](@ref) type struct
+- `envir` [`AirLayer`](@ref) type struct
+"""
+function update_leaf_AK!(
+            photo_set::AbstractPhotoModelParaSet{FT},
+            leaves::Leaves{FT},
+            envir::AirLayer{FT}
+            ) where {FT<:AbstractFloat}
+    # unpack required variables
+    @unpack APAR, ec, g_bc, g_bw, g_m, g_max, g_min, p_sat = leaves;
+    @unpack p_atm, p_H₂O = envir;
+
+    # calculate the physiological maximal g_sw
+    _g_crit = ec / (p_sat - p_H₂O) * p_atm;
+    _g_sw   = 1 ./ max.(1/_g_crit .- 1 ./ g_bw, FT(1e-3));
+    _g_sw  .= min.(_g_sw, g_max);
+    _g_sw  .= max.(_g_sw, g_min);
+    _g_lcs  = 1 ./ (1 ./ g_bc .+ FT(1.6) ./ _g_sw .+ 1 ./ g_m);
+
+    # update the limited rates
+    _Jps = leaf_ETR_pot_APAR(leaves.ps, APAR);
+    _Js  = leaf_ETR_Jps(photo_set, leaves.ps, _Jps);
+    _Anc = rubisco_limited_an_glc(photo_set, leaves.ps, envir, _g_lcs);
+    _Anj = light_limited_an_glc(photo_set, leaves.ps, envir, _g_lcs, _Js);
+    _Anp = product_limited_an_glc(photo_set, leaves.ps, envir, _g_lcs);
+    _Ams = min.(_Anc, _Anj, _Anp);
+
+    # update a_max and kr_max
+    leaves.a_max  .= _Ams;
+    leaves.kr_max  = leaves.hs.k_history[end];
+
+    return nothing
+end
+
+
+
+
+
+
+
+
+###############################################################################
+#
+# Update Leaves struct from stomatal conductance
+#
+###############################################################################
+"""
+    update_leaf_from_glc!(
+            photo_set::AbstractPhotoModelParaSet{FT},
+            leaves::Leaves{FT},
+            envir::AirLayer{FT},
+            ind::Int,
+            glc::FT)
+
+Update Nth leaf photosynthesis, given
+- `photo_set` [`C3ParaSet`](@ref) or [`C4ParaSet`](@ref) type parameter set
+- `leaves` [`Leaves`](@ref) type struct
+- `envir` [`AirLayer`](@ref) type struct
+- `ind` Nth leaf
+- `glc` Given leaf diffusive conductance
+"""
+function update_leaf_from_glc!(
+            photo_set::AbstractPhotoModelParaSet{FT},
+            leaves::Leaves{FT},
+            envir::AirLayer{FT},
+            ind::Int,
+            glc::FT
+            ) where {FT<:AbstractFloat}
+    # update the conductances
+    leaves.g_lc[ind] = glc;
+    leaves.g_sc[ind] = 1 / ( 1 / glc -
+                             1 / leaves.g_m[ind] -
+                             1 / leaves.g_bc[ind] );
+    leaves.g_sw[ind] = leaves.g_sc[ind] * FT(1.6);
+    leaves.g_lw[ind] = 1 / ( 1 / leaves.g_sw[ind] +
+                             1 / leaves.g_bw[ind] );
+
+    # update the photosynthetic rates
+    if glc != leaves.ps.g_lc
+        leaf_photo_from_glc!(photo_set, leaves.ps, envir, glc);
+    end
+    leaves.Ac[ind] = leaves.ps.Ac;
+    leaves.Aj[ind] = leaves.ps.Aj;
+    leaves.Ap[ind] = leaves.ps.Ap;
+    leaves.Ag[ind] = leaves.ps.Ag;
+    leaves.An[ind] = leaves.ps.An;
+
+    # update the pressures
+    leaves.p_i[ind] = leaves.ps.p_i;
+    leaves.p_s[ind] = leaves.ps.p_s;
+
+    return nothing
+end
+
+
+
+
+"""
+    update_leaf_from_gsw!(
+            photo_set::AbstractPhotoModelParaSet{FT},
+            leaves::Leaves{FT},
+            envir::AirLayer{FT},
+            ind::Int,
+            glc::FT)
+
+Update Nth leaf photosynthesis, given
+- `photo_set` [`C3ParaSet`](@ref) or [`C4ParaSet`](@ref) type parameter set
+- `leaves` [`Leaves`](@ref) type struct
+- `envir` [`AirLayer`](@ref) type struct
+- `ind` Nth leaf
+- `gsw` Given stomatal conductance to H₂O
+"""
+function update_leaf_from_gsw!(
+            photo_set::AbstractPhotoModelParaSet{FT},
+            leaves::Leaves{FT},
+            envir::AirLayer{FT},
+            ind::Int,
+            gsw::FT
+            ) where {FT<:AbstractFloat}
+    # update the conductances
+    leaves.g_sw[ind] = gsw;
+    leaves.g_lw[ind] = 1 / ( 1 / gsw +
+                             1 / leaves.g_bw[ind] );
+    leaves.g_sc[ind] = gsw / FT(1.6);
+    leaves.g_lc[ind] = 1 / ( FT(1.6) / gsw +
+                             1 / leaves.g_m[ind] +
+                             1 / leaves.g_bc[ind] );
+
+    # update the photosynthetic rates
+    leaf_photo_from_glc!(photo_set, leaves.ps, envir, leaves.g_lc[ind]);
+    leaves.Ac[ind] = leaves.ps.Ac;
+    leaves.Aj[ind] = leaves.ps.Aj;
+    leaves.Ap[ind] = leaves.ps.Ap;
+    leaves.Ag[ind] = leaves.ps.Ag;
+    leaves.An[ind] = leaves.ps.An;
+
+    # update the pressures
+    leaves.p_i[ind] = leaves.ps.p_i;
+    leaves.p_s[ind] = leaves.ps.p_s;
+
+    return nothing
+end
+
+
+
+
+
+
+
+
+###############################################################################
+#
+# Make g_min and g_max control
+#
+###############################################################################
+"""
+    leaf_gsw_control!(
+            photo_set::AbstractPhotoModelParaSet,
+            leaves::Leaves{FT},
+            envir::AirLayer{FT},
+            ind::Int)
+
+make sure g_sw is in its physiological range limited by diffusion, given
+- `photo_set` [`C3ParaSet`](@ref) or [`C4ParaSet`](@ref) type parameter set
+- `leaves` [`Leaves`](@ref) type struct
+- `envir` [`AirLayer`](@ref) type struct
+"""
+function leaf_gsw_control!(
+            photo_set::AbstractPhotoModelParaSet,
+            leaves::Leaves{FT},
+            envir::AirLayer{FT},
+            ind::Int
+            ) where {FT<:AbstractFloat}
+    # if g_sw is low than g_min
+    if leaves.g_sw[ind] < leaves.g_min
+        update_leaf_from_gsw!(photo_set, leaves, envir, ind, leaves.g_min);
+    elseif leaves.g_sw[ind] > leaves.g_max
+        update_leaf_from_gsw!(photo_set, leaves, envir, ind, leaves.g_max);
+    end
+
+    return nothing
+end
---
+++ test/runtests.jl
@@ -0,0 +1,168 @@
+using Photosynthesis
+using StomataModels
+using Test
+
+PM = Photosynthesis
+SM = StomataModels
+
+
+
+
+# Test the variable FT recursively
+function recursive_FT_test(para, FT)
+    # if the type is AbstractFloat
+    if typeof(para) <: AbstractFloat
+        try
+            @test typeof(para) == FT
+        catch e
+            println("The not NaN test failed for ", para, " and ", FT)
+        end
+    # if the type is array
+    elseif typeof(para) <: AbstractArray
+        if eltype(para) <: AbstractFloat
+            try
+                @test eltype(para) == FT
+            catch e
+                println("The not NaN test failed for ", para, " and ", FT)
+            end
+        else
+            for ele in para
+                recursive_FT_test(ele, FT)
+            end
+        end
+    else
+        # try if the parameter is a struct
+        try
+            for fn in fieldnames( typeof(para) )
+                recursive_FT_test( getfield(para, fn), FT )
+            end
+        catch e
+            println(typeof(para), "is not supprted by recursive_FT_test.")
+        end
+    end
+end
+
+
+
+
+# Test the variable NaN recursively
+function recursive_NaN_test(para)
+    # if the type is Number
+    if typeof(para) <: Number
+        try
+            @test !isnan(para)
+        catch e
+            println("The not NaN test failed for", para)
+        end
+    # if the type is array
+    elseif typeof(para) <: AbstractArray
+        for ele in para
+            recursive_NaN_test(ele)
+        end
+    else
+        # try if the parameter is a struct
+        try
+            for fn in fieldnames( typeof(para) )
+                recursive_NaN_test( getfield(para, fn) )
+            end
+        catch e
+            println(typeof(para), "is not supprted by recursive_NaN_test.")
+        end
+    end
+end
+
+
+
+
+# FT and NaN test
+@testset "StomtaModels --- FT consistency and not NaN" begin
+    for FT in [Float32, Float64]
+        envir  = PM.AirLayer{FT}();
+        leaf_3 = PM.Leaf{FT}();
+        leaf_4 = PM.Leaf{FT}();
+        mod_3  = PM.C3CLM(FT);
+        mod_4  = PM.C4CLM(FT);
+        rand_T = rand(FT) + 298;
+        lv_3   = SM.Leaves{FT}(n_leaf=2);
+        lv_4   = SM.Leaves{FT}(n_leaf=2);
+        esm_1  = SM.ESMBallBerry{FT}();
+        esm_2  = SM.ESMGentine{FT}();
+        esm_3  = SM.ESMLeuning{FT}();
+        esm_4  = SM.ESMMedlyn{FT}();
+        osm_1  = SM.OSMEller{FT}();
+        osm_2  = SM.OSMSperry{FT}();
+        osm_3  = SM.OSMWang{FT}();
+        osm_4  = SM.OSMWAP{FT}();
+        osm_5  = SM.OSMWAPMod{FT}();
+
+        # test the structures
+        for result in [ lv_3, esm_1, esm_2, esm_3, esm_4, osm_4, osm_5 ]
+            recursive_FT_test(result, FT);
+            recursive_NaN_test(result);
+        end
+
+        # test the refresh functions
+        SM.update_leaf_TP!(mod_3, lv_3, envir);
+        SM.update_leaf_TP!(mod_4, lv_4, envir);
+        SM.update_leaf_AK!(mod_3, lv_3, envir);
+        SM.update_leaf_AK!(mod_4, lv_4, envir);
+        SM.update_leaf_from_glc!(mod_3, lv_3, envir, 1, FT(0.1));
+        SM.update_leaf_from_glc!(mod_4, lv_4, envir, 1, FT(0.1));
+        SM.update_leaf_from_gsw!(mod_3, lv_3, envir, 1, FT(0.05));
+        SM.update_leaf_from_gsw!(mod_4, lv_4, envir, 1, FT(0.05));
+        lv_3.g_sw[2] = 0;
+        lv_4.g_sw[2] = 0;
+        SM.leaf_gsw_control!(mod_3, lv_3, envir, 2);
+        SM.leaf_gsw_control!(mod_4, lv_4, envir, 2);
+        recursive_NaN_test(lv_3);
+        recursive_NaN_test(lv_4);
+
+        # test the empirical model formulations
+        for result in [ SM.empirical_gsw_from_model(esm_1, leaf_3, envir, FT(1)),
+                        SM.empirical_gsw_from_model(esm_2, leaf_3, envir, FT(1)),
+                        SM.empirical_gsw_from_model(esm_3, leaf_3, envir, FT(1)),
+                        SM.empirical_gsw_from_model(esm_4, leaf_3, envir, FT(1)),
+                        SM.empirical_gsw_from_model(esm_1, lv_3, envir, FT(1)),
+                        SM.empirical_gsw_from_model(esm_2, lv_3, envir, FT(1)),
+                        SM.empirical_gsw_from_model(esm_3, lv_3, envir, FT(1)),
+                        SM.empirical_gsw_from_model(esm_4, lv_3, envir, FT(1)),
+                        SM.empirical_gsw_from_model(esm_1, lv_4, envir, FT(1)),
+                        SM.empirical_gsw_from_model(esm_2, lv_4, envir, FT(1)),
+                        SM.empirical_gsw_from_model(esm_3, lv_4, envir, FT(1)),
+                        SM.empirical_gsw_from_model(esm_4, lv_4, envir, FT(1)),
+                        SM.empirical_gsw_from_model(esm_1, lv_3, envir, FT(1), 1),
+                        SM.empirical_gsw_from_model(esm_2, lv_3, envir, FT(1), 1),
+                        SM.empirical_gsw_from_model(esm_3, lv_3, envir, FT(1), 1),
+                        SM.empirical_gsw_from_model(esm_4, lv_3, envir, FT(1), 1),
+                        SM.empirical_gsw_from_model(esm_1, lv_4, envir, FT(1), 1),
+                        SM.empirical_gsw_from_model(esm_2, lv_4, envir, FT(1), 1),
+                        SM.empirical_gsw_from_model(esm_3, lv_4, envir, FT(1), 1),
+                        SM.empirical_gsw_from_model(esm_4, lv_4, envir, FT(1), 1) ]
+            recursive_FT_test(result, FT);
+            recursive_NaN_test(result);
+        end
+
+        # test the solution functions
+        for (mod,lv) in zip([mod_3, mod_3], [lv_3, lv_4])
+            for sm in [esm_1, esm_2, esm_3, esm_4, osm_1, osm_2, osm_3, osm_4, osm_5]
+                result = SM.envir_diff!(FT(0.1), mod, lv, envir, sm, 1);
+                recursive_FT_test(result, FT);
+                recursive_NaN_test(result);
+            end
+        end
+
+        # test the stomata solutions
+        for (mod,lv) in zip([mod_3, mod_3], [lv_3, lv_4])
+            for sm in [esm_1, esm_2, esm_3, esm_4, osm_1, osm_2, osm_3, osm_4, osm_5]
+                SM.leaf_photo_from_envir!(mod, lv, envir, sm, 1);
+                recursive_NaN_test(lv);
+            end
+        end
+        for (mod,lv) in zip([mod_3, mod_3], [lv_3, lv_4])
+            for sm in [esm_1, esm_2, esm_3, esm_4, osm_1, osm_2, osm_3, osm_4, osm_5]
+                SM.leaf_photo_from_envir!(mod, lv, envir, sm);
+                recursive_NaN_test(lv);
+            end
+        end
+    end
+end
