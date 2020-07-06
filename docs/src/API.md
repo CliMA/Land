f1fea3264944ebe661ad3ad7e17ae28eb5250c40
Yujie Wang <jesiner@gmail.com>
2020-07-06 12:02:31 -0700

    add docs

---
+++ docs/src/API.md
@@ -0,0 +1,41 @@
+# StomtaModels API
+```@meta
+CurrentModule = StomataModels
+```
+
+## Stomatal model schemes
+```@docs
+AbstractStomatalModel
+EmpiricalStomatalModel
+ESMBallBerry
+ESMGentine
+ESMLeuning
+ESMMedlyn
+OptimizationStomatalModel
+OSMEller
+OSMSperry
+OSMWang
+OSMWAP
+OSMWAPMod
+```
+
+## Leaves
+```@docs
+Leaves
+```
+
+## Stomatal conductance
+```@docs
+empirical_gsw_from_model
+envir_diff!
+leaf_photo_from_envir!
+```
+
+## Photosynthesis
+```@docs
+update_leaf_TP!
+update_leaf_AK!
+update_leaf_from_glc!
+update_leaf_from_gsw!
+leaf_gsw_control!
+```
--- src/StomataModels.jl
+++ src/StomataModels.jl
@@ -8,16 +8,43 @@ using Photosynthesis
 using PlantHydraulics
 using WaterPhysics
 
-Planet = CLIMAParameters.Planet
-
 
 
 
 # Define constants
+Planet = CLIMAParameters.Planet
 struct EarthParameterSet <: AbstractEarthParameterSet end
-const EARTH = EarthParameterSet()
+const EARTH         = EarthParameterSet()
 const K_25          = Planet.T_freeze(EARTH) + 25
 const MOLMASS_WATER = Planet.molmass_water(EARTH)
+
+
+
+
+# export public types and structs
+export AbstractStomatalModel,
+       EmpiricalStomatalModel,
+       ESMBallBerry,
+       ESMGentine,
+       ESMLeuning,
+       ESMMedlyn,
+       Leaves,
+       OptimizationStomatalModel,
+       OSMEller,
+       OSMSperry,
+       OSMWang,
+       OSMWAP,
+       OSMWAPMod
+
+# export public functions
+export empirical_gsw_from_model,
+       envir_diff!,
+       leaf_gsw_control!,
+       leaf_photo_from_envir!,
+       update_leaf_AK!,
+       update_leaf_from_glc!,
+       update_leaf_from_gsw!,
+       update_leaf_TP!
 
 
 
