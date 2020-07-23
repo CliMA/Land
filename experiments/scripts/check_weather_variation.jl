99af29b560f9ae7a5d6140bd822f8e52f8798c63
Yujie Wang <jesiner@gmail.com>
2020-07-23 15:47:35 -0700

    move script files to ResearchProject repository

--- experiments/scripts/check_weather_variation.jl
+++
@@ -1,9 +0,0 @@
-# include("./scripts/check_weather_variation.jl")
-weat_years = CSV.read("./data/Flagstaff/histo_30_amb.csv")
-for year in 1971:2005
-    weat_mask = (weat_years.Year .== year)
-    weat_year = weat_years[weat_mask,:]
-    mgst = mean(weat_year.Tair)
-    sgsr = sum(weat_year.Rain)
-    println(year, "\t", mgst, "\t", sgsr)
-end
