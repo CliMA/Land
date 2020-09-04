653d328c1cae167b952248a30d8b511dcf6953bb
Yujie Wang <jesiner@gmail.com>
2020-09-04 16:08:50 -0700

    version 0.1.0

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,68 +5,7 @@ using Test
 
 
 
-# Test the variable FT recursively
-function recursive_FT_test(para, FT)
-    # if the type is AbstractFloat
-    if typeof(para) <: AbstractFloat
-        try
-            @test typeof(para) == FT
-        catch e
-            println("The not NaN test failed for ", para, " and ", FT)
-        end
-    # if the type is array
-    elseif typeof(para) <: AbstractArray
-        if eltype(para) <: AbstractFloat
-            try
-                @test eltype(para) == FT
-            catch e
-                println("The not NaN test failed for ", para, " and ", FT)
-            end
-        else
-            for ele in para
-                recursive_FT_test(ele, FT)
-            end
-        end
-    else
-        # try if the parameter is a struct
-        try
-            for fn in fieldnames( typeof(para) )
-                recursive_FT_test( getfield(para, fn), FT )
-            end
-        catch e
-            println(typeof(para), "is not supprted by recursive_FT_test.")
-        end
-    end
-end
-
-
-
-
-# Test the variable NaN recursively
-function recursive_NaN_test(para)
-    # if the type is Number
-    if typeof(para) <: Number
-        try
-            @test !isnan(para)
-        catch e
-            println("The not NaN test failed for", para)
-        end
-    # if the type is array
-    elseif typeof(para) <: AbstractArray
-        for ele in para
-            recursive_NaN_test(ele)
-        end
-    else
-        # try if the parameter is a struct
-        try
-            for fn in fieldnames( typeof(para) )
-                recursive_NaN_test( getfield(para, fn) )
-            end
-        catch e
-            println(typeof(para), "is not supprted by recursive_NaN_test.")
-        end
-    end
-end
+include("recursive_test.jl")
 
 
 
