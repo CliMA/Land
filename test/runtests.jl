7be1b9f1326954feb4fb5caa27d0313f2b8c1014
Yujie Wang <jesiner@gmail.com>
2020-11-19 15:44:57 -0800

    redo tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,12 @@
-using BenchmarkTools
 using PlantHydraulics
 using Test
 
 
 
 
-include("recursive_test.jl")
+include("recursive.jl")
 
 
 
 
-benchmarking = false
-include("test_struct.jl")
-include("test_vc_soil.jl")
-include("test_vc_xylem.jl")
-include("test_leaf.jl")
-include("test_legacy.jl")
-include("test_temperature.jl")
-include("test_root.jl")
-include("test_pressure.jl")
-include("test_plant.jl")
-
-benchmarking = true
+include("test_PlantHydraulics.jl")
