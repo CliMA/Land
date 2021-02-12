c4806b41abf81993b5a5d2e55b003d4b3511ffbe
Yujie Wang <jesiner@gmail.com>
2021-02-12 14:50:50 -0800

    add sustained NPQ to fluorescence model

--- src/fluorescence/fluorescence.jl
+++ src/fluorescence/fluorescence.jl
@@ -9,7 +9,7 @@
                 leaf::Leaf{FT}
     ) where {FT<:AbstractFloat}
 
-Compute fluorescence yield, Kn, and Kp for leaf, given
+Compute fluorescence yield, Kr, Ks, and Kp for leaf, given
 - `fluo_set` [`FluoParaSet`](@ref) type parameter set
 - `leaf` [`Leaf`](@ref) struct
 """
@@ -17,8 +17,8 @@ function leaf_fluorescence!(
             fluo_set::FluoParaSet{FT},
             leaf::Leaf{FT}
 ) where {FT<:AbstractFloat}
-    @unpack Ag, Kd, Kf, maxPSII = leaf;
-    @unpack Kn1, Kn2, Kn3 = fluo_set;
+    @unpack Ag, Kd, Kf, Kp_max, maxPSII = leaf;
+    @unpack Kr1, Kr2, Kr3 = fluo_set;
 
     # Actual effective ETR:
     leaf.Ja  = max(0, Ag / leaf.CO₂_per_electron);
@@ -35,25 +35,27 @@ function leaf_fluorescence!(
     x        = max(0,  1-leaf.φ/maxPSII);
 
     # Max PSII rate constant
-    Kp_max   = FT(4.0);
+    x_α      = exp(log(x)*Kr2);
+    leaf.Kr  = Kr1 * (1 + Kr3)* x_α / (Kr3 + x_α);
+    leaf.Kp  = max(0, leaf.φ*(Kf+Kd+leaf.Kr)/(1-leaf.φ));
 
-    x_alpha  = exp(log(x)*Kn2);
-    #println(x_alpha)
+    leaf.Fo  = Kf / (Kf+Kp_max+Kd                );
+    leaf.Fo′ = Kf / (Kf+Kp_max+Kd+leaf.Kr+leaf.Ks);
+    leaf.Fm  = Kf / (Kf       +Kd                );
+    leaf.Fm′ = Kf / (Kf       +Kd+leaf.Kr+leaf.Ks);
+    leaf.ϕs  = leaf.Fm′ * (1 - leaf.φ);
 
-    leaf.Kn  = Kn1 * (1+Kn3)* x_alpha/(Kn3 + x_alpha);
-    leaf.Kp  = max(0,leaf.φ*(Kf+Kd+leaf.Kn)/(1-leaf.φ));
-
-    leaf.Fo  = Kf/(Kf+Kp_max+Kd        );
-    leaf.Fo′ = Kf/(Kf+Kp_max+Kd+leaf.Kn);
-    leaf.Fm  = Kf/(Kf       +Kd        );
-    leaf.Fm′ = Kf/(Kf       +Kd+leaf.Kn);
-    leaf.ϕs  = leaf.Fm′*(1-leaf.φ);
+    #
+    #
+    # TODO should these change with Ks?
+    #
+    #
     # leaf.eta  = leaf.ϕs/leaf.Fo
     # don't need this anymore
     # better to use ϕs directly for SIF as Fo is not always fqe=0.01
-    leaf.qQ  = 1-(leaf.ϕs-leaf.Fo′)/(leaf.Fm-leaf.Fo′);
-    leaf.qE  = 1-(leaf.Fm-leaf.Fo′)/(leaf.Fm′-leaf.Fo);
-    leaf.NPQ = leaf.Kn/(Kf+Kd);
+    leaf.qQ  = 1 - (leaf.ϕs-leaf.Fo′) / (leaf.Fm-leaf.Fo′);
+    leaf.qE  = 1 - (leaf.Fm-leaf.Fo′) / (leaf.Fm′-leaf.Fo);
+    leaf.NPQ = leaf.Kr / (Kf + Kd);
 
     return nothing
 end
