bd4ddfbf7d103b0a0a29388ab156654883013788
Yujie Wang <jesiner@gmail.com>
2022-02-11 13:36:56 -0800

    use ClimaCache v0.2

--- src/colimit.jl
+++ src/colimit.jl
@@ -2,7 +2,7 @@
 #
 # Changes to this function
 # General
-#     2022-Jan-14: add colimt function back
+#     2022-Jan-14: add colimit function back
 #
 #######################################################################################################################################################################################################
 """
@@ -27,11 +27,11 @@ function colimit_photosynthesis! end
 
     colimit_photosynthesis!(psm::Union{C3CytochromeModel{FT}, C3VJPModel{FT}, C4VJPModel{FT}}) where {FT<:AbstractFloat}
 
-Use the minimal photosynthetic rates of rubisco-, light-, and product-limited photosynthetic rates, given
+Colimit the photosynthesis by rubisco-, light-, and product-limited photosynthetic rates, given
 - `psm` `C3VJPModel` or `C4VJPModel` type photosynthesis model
 """
 colimit_photosynthesis!(psm::Union{C3CytochromeModel{FT}, C3VJPModel{FT}, C4VJPModel{FT}}) where {FT<:AbstractFloat} = (
-    colimit_photosynthesis!(psm, psm.COLIMIT);
+    colimit_photosynthesis!(psm, psm.COLIMIT_CJ, psm.COLIMIT_IP);
 
     return nothing
 );
@@ -41,21 +41,29 @@ colimit_photosynthesis!(psm::Union{C3CytochromeModel{FT}, C3VJPModel{FT}, C4VJPM
 #
 # Changes to this method
 # General
-#     2022-Jan-14: add colimt function back
-#     2022-Jan-24: fix documentation
-#     2022-Feb-07: add C3Cytochrome to method (colimit j_p680 and j_p700 as well)
+#     2022-Feb-11: add a wrapper function to step through COLIMIT_CJ and COLIMIT_IP
 #
 #######################################################################################################################################################################################################
 """
 
-    colimit_photosynthesis!(psm::Union{C3CytochromeModel{FT}, C3VJPModel{FT}, C4VJPModel{FT}}, colim::MinimumColimit{FT}) where {FT<:AbstractFloat}
+    colimit_photosynthesis!(
+                psm::Union{C3CytochromeModel{FT}, C3VJPModel{FT}, C4VJPModel{FT}},
+                colim_cj::Union{MinimumColimit{FT}, QuadraticColimit{FT}},
+                colim_ip::Union{MinimumColimit{FT}, QuadraticColimit{FT}}
+    ) where {FT<:AbstractFloat}
 
-Use the minimal photosynthetic rates of rubisco-, light-, and product-limited photosynthetic rates, given
-- `psm` `C3CytochromeModel`, `C3VJPModel` or `C4VJPModel` type photosynthesis model
-- `colim` `MinimumColimit` type struct
+Colimit the photosynthesis by rubisco-, light-, and product-limited photosynthetic rates, given
+- `psm` `C3VJPModel` or `C4VJPModel` type photosynthesis model
+- `colim_cj` Colimit method for rubisco- and light-limited rates
+- `colim_ip` Colimit method for the colimited rubisco- and light-limited rate and product-limited rate
 """
-colimit_photosynthesis!(psm::Union{C3CytochromeModel{FT}, C3VJPModel{FT}, C4VJPModel{FT}}, colim::MinimumColimit{FT}) where {FT<:AbstractFloat} = (
-    psm.a_gross = min(psm.a_c, psm.a_j, psm.a_p);
+colimit_photosynthesis!(
+            psm::Union{C3CytochromeModel{FT}, C3VJPModel{FT}, C4VJPModel{FT}},
+            colim_cj::Union{MinimumColimit{FT}, QuadraticColimit{FT}},
+            colim_ip::Union{MinimumColimit{FT}, QuadraticColimit{FT}}
+) where {FT<:AbstractFloat} = (
+    _a_i        = colimited_rate(psm.a_c, psm.a_j, colim_cj);
+    psm.a_gross = colimited_rate(psm.a_p, _a_i, colim_ip);
     psm.a_net   = psm.a_gross - psm.r_d;
 
     return nothing
@@ -64,30 +72,60 @@ colimit_photosynthesis!(psm::Union{C3CytochromeModel{FT}, C3VJPModel{FT}, C4VJPM
 
 #######################################################################################################################################################################################################
 #
+# Changes to this function
+# General
+#     2022-Feb-11: add colimited_rate for general purpose in ETR as well as a_gross
+#
+#######################################################################################################################################################################################################
+"""
+This function returns the colimited rates from two rates. Supported methods are
+
+$(METHODLIST)
+
+"""
+function colimited_rate end
+
+
+#######################################################################################################################################################################################################
+#
 # Changes to this method
 # General
-#     2022-Jan-14: add colimt function back
+#     2022-Jan-14: add colimit function back
+#     2022-Jan-24: fix documentation
+#     2022-Feb-07: add C3Cytochrome to method (colimit j_p680 and j_p700 as well)
+#     2022-Feb-11: refactor this function to return the minimum of two rates
+#
+#######################################################################################################################################################################################################
+"""
+
+    colimited_rate(a_1::FT, a_2::FT, colim::MinimumColimit{FT}) where {FT<:AbstractFloat}
+
+Return the minimum of two rates, given
+- `a_1` Rate 1
+- `a_2` Rate 2
+- `colim` `MinimumColimit` type struct
+"""
+colimited_rate(a_1::FT, a_2::FT, colim::MinimumColimit{FT}) where {FT<:AbstractFloat} = min(a_1, a_2);
+
+
+#######################################################################################################################################################################################################
+#
+# Changes to this method
+# General
+#     2022-Jan-14: add colimit function back
 #     2022-Jan-14: unpack CONSTANT from the input variables only
 #     2022-Jan-24: fix documentation
 #     2022-Feb-07: add C3Cytochrome to method (colimit j_p680 and j_p700 as well)
+#     2022-Feb-11: refactor this function to return the quadratic colimitation of two rates
 #
 #######################################################################################################################################################################################################
 """
 
-    colimit_photosynthesis!(psm::Union{C3CytochromeModel{FT}, C3VJPModel{FT}, C4VJPModel{FT}}, colim::QuadraticColimit{FT}) where {FT<:AbstractFloat}
+    colimited_rate(a_1::FT, a_2::FT, colim::QuadraticColimit{FT}) where {FT<:AbstractFloat}
 
-Use the minimal photosynthetic rates of rubisco-, light-, and product-limited photosynthetic rates, given
-- `psm` `C3CytochromeModel`, `C3VJPModel` or `C4VJPModel` type photosynthesis model
+Return the quadratic colimitation of two rates, given
+- `a_1` Rate 1
+- `a_2` Rate 2
 - `colim` `QuadraticColimit` type struct
 """
-colimit_photosynthesis!(psm::Union{C3CytochromeModel{FT}, C3VJPModel{FT}, C4VJPModel{FT}}, colim::QuadraticColimit{FT}) where {FT<:AbstractFloat} = (
-    @unpack CURVATURE = colim;
-
-    _a = lower_quadratic(CURVATURE, -(psm.a_c + psm.a_j), psm.a_c * psm.a_j);
-    _a = lower_quadratic(CURVATURE, -(_a + psm.a_p), _a  * psm.a_p);
-
-    psm.a_gross = isnan(_a) ? min(psm.a_c, psm.a_j, psm.a_p) : _a;
-    psm.a_net   = psm.a_gross - psm.r_d;
-
-    return nothing
-);
+colimited_rate(a_1::FT, a_2::FT, colim::QuadraticColimit{FT}) where {FT<:AbstractFloat} = lower_quadratic(colim.CURVATURE, -(a_1 + a_2), a_1 * a_2);
