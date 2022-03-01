0a43044d1e7faa0f0d1b62adbdce62d7cd7a114b
Yujie Wang <jesiner@gmail.com>
2022-02-28 19:29:34 -0800

    add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -27,9 +27,14 @@ using Test
             leaf_2 = Leaf{FT}("C3Cytochrome"; colimit = true);
             air    = AirLayer{FT}();
             p_mode = PCO₂Mode();
+            g_mode = GCO₂Mode();
             leaf_photosynthesis!(leaf_1, air, p_mode);
             @test true;
+            leaf_photosynthesis!(leaf_1, air, g_mode);
+            @test true;
             leaf_photosynthesis!(leaf_2, air, p_mode);
+            @test true;
+            leaf_photosynthesis!(leaf_2, air, g_mode);
             @test true;
         end;
     end;
@@ -49,6 +54,25 @@ using Test
             @test true;
             leaf_photosynthesis!(leaf_2, air, g_mode);
             @test true;
+        end;
+    end;
+
+    @testset "P&G Modes" begin
+        for FT in [Float32, Float64]
+            air    = AirLayer{FT}();
+            p_mode = PCO₂Mode();
+            g_mode = GCO₂Mode();
+            for LT in ["C3", "C4", "C3Cytochrome"]
+                leaf_1 = Leaf{FT}(LT);
+                leaf_2 = Leaf{FT}(LT);
+                for glc in collect(0.05:0.05:0.3)
+                    leaf_photosynthesis!(leaf_1, air, g_mode, FT(glc));
+                    leaf_photosynthesis!(leaf_2, air, p_mode, leaf_1.p_CO₂_i);
+                    @test leaf_1.PSM.a_gross ≈ leaf_2.PSM.a_gross;
+                    @test leaf_1.PSM.e_to_c ≈ leaf_2.PSM.e_to_c;
+                    @test leaf_1.PRC.ϕ_f ≈ leaf_2.PRC.ϕ_f;
+                end;
+            end;
         end;
     end;
 end;
