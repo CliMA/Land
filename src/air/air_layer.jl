1366715459110f55e202e7378cb4dfa64be9fa5f
Yujie Wang <jesiner@gmail.com>
2022-04-19 10:52:43 -0700

    update AirLayer struct

--- src/air/air_layer.jl
+++ src/air/air_layer.jl
@@ -7,6 +7,7 @@
 #     2022-Jan-14: add p_CO₂ to the structure
 #     2022-Jan-24: fix documentation
 #     2022-Mar-09: add t, p_H₂O, p_H₂O_sat, rh, and wind fields
+#     2022-Apr-19: move p_H₂O and wind to prognostic fields
 #
 #######################################################################################################################################################################################################
 """
@@ -30,18 +31,18 @@ mutable struct AirLayer{FT<:AbstractFloat}
     # prognostic variables that change with time
     "CO₂ partial pressure `[Pa]`"
     p_CO₂::FT
-    "Temperature"
-    t::FT
-
-    # diagnodtic variables that change with time
     "H₂O partial pressure `[Pa]`"
     p_H₂O::FT
+    "Temperature"
+    t::FT
+    "Wind speed `[m s⁻¹]`"
+    wind::FT
+
+    # diagnodtic variables that change with time
     "Saturated H₂O partial pressure `[Pa]`"
     p_H₂O_sat::FT
     "relative humidity"
     rh::FT
-    "Wind speed `[m s⁻¹]`"
-    wind::FT
 end
 
 
@@ -52,6 +53,7 @@ end
 #     2022-Jan-14: Move the structure from Photosynthesis.jl, only P_A and P_O2 for now
 #     2022-Jan-24: add p_CO₂ to the constructors
 #     2022-Mar-09: add t, p_H₂O, p_H₂O_sat, rh, and wind fields
+#     2022-Apr-19: move p_H₂O and wind to prognostic fields
 #
 #######################################################################################################################################################################################################
 """
@@ -71,9 +73,9 @@ AirLayer{FT}() where {FT<:AbstractFloat} = (
                 P_ATM(FT),                                  # P_AIR
                 P_ATM(FT) * 0.209,                          # P_O₂
                 40,                                         # p_CO₂
-                T_25(FT),                                   # t
                 1500,                                       # p_H₂O
+                T_25(FT),                                   # t
+                1,                                          # wind
                 saturation_vapor_pressure(T_25(FT)),        # p_H₂O_sat
-                1500 / saturation_vapor_pressure(T_25(FT)), # rh
-                1)                                          # wind
+                1500 / saturation_vapor_pressure(T_25(FT))) # rh
 );
