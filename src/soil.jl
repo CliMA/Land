f2bff2755d20b078c9b0553dac1b8d1fa4006b4e
Yujie Wang <jesiner@gmail.com>
2022-06-14 15:45:56 -0700

    prototype soil albedo functions

---
+++ src/soil.jl
@@ -0,0 +1,15 @@
+#######################################################################################################################################################################################################
+#
+# Changes to this function
+# General
+#     2022-Jun-14: migrate the function from CanopyLayers
+#     2022-Jun-09: rename function to soil_albedo!
+#
+#######################################################################################################################################################################################################
+"""
+This function updates soil albedo values for shortwave PAR and NIR. The supported methods include
+
+$(METHODLIST)
+
+"""
+function soil_albedo! end
