97217e8176820c24d625c6ce983ed340ff87307e
Yujie Wang <jesiner@gmail.com>
2022-07-01 11:18:18 -0700

    migrate gsw limits function

--- src/StomataModels.jl
+++ src/StomataModels.jl
@@ -1,11 +1,11 @@
 module StomataModels
 
-using ClimaCache: AbstractSoilVC, AbstractXylemVC
+using ClimaCache: AbstractSoilVC, AbstractXylemVC, Leaf, Leaves1D, Leaves2D
 using DocStringExtensions: METHODLIST
 using PlantHydraulics: relative_hydraulic_conductance
 using SoilHydraulics: relative_hydraulic_conductance
 using UnPack: @unpack
-using WaterPhysics: relative_surface_tension
+using WaterPhysics: relative_diffusive_coefficient, relative_surface_tension
 
 
 # include files
--- test/runtests.jl
+++ test/runtests.jl
@@ -16,4 +16,16 @@ using Test
             @test 0 < StomataModels.β_factor(f_2, swc) < 1;
         end;
     end;
+
+    @testset "Conductance Limits" begin
+        for FT in [Float32, Float64]
+            lf_1 = ClimaCache.Leaf{FT}("C3");
+            lf_2 = ClimaCache.Leaves1D{FT}("C3");
+            lf_3 = ClimaCache.Leaves2D{FT}("C3");
+            for lf in [lf_1, lf_2, lf_3]
+                StomataModels.limit_stomatal_conductance!(lf);
+                @test true;
+            end;
+        end;
+    end;
 end;
