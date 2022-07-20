d9f84ba22ac8f5907d8bd70ea8f2c43f7c98ea55
Yujie Wang <jesiner@gmail.com>
2022-07-20 14:46:42 -0700

    add more fields to Meteorology

--- src/air/meteorology.jl
+++ src/air/meteorology.jl
@@ -4,6 +4,7 @@
 # General
 #     2022-Jul-14: add Meteorology struct to store meteorological data
 #     2022-Jul-14: add field t_precip
+#     2022-Jul-20: add field t_air
 #
 #######################################################################################################################################################################################################
 """
@@ -18,11 +19,13 @@ $(TYPEDFIELDS)
 
 """
 Base.@kwdef mutable struct Meteorology{FT<:AbstractFloat}
-    # prognostic variables that change with time
+    # Prognostic variables
     "Precipitation in form of rain (before interception) `[mol m⁻²]`"
     rain::FT = 0
     "Precipitation in form of snow (before interception) `[mol m⁻²]`"
     snow::FT = 0
+    "Air temperature as the boundary condition for canopy airspace `[K]`"
+    t_air::FT = T₂₅()
     "Precipitation temperature `[K]`"
     t_precip::FT = T₂₅()
 end
