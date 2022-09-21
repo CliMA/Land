ee51034800d7d17428487a546a9e8c7de7d020eb
Yujie Wang <jesiner@gmail.com>
2022-09-21 14:40:00 -0700

    use const from EmeraldConstants

--- src/LeafOptics.jl
+++ src/LeafOptics.jl
@@ -2,7 +2,7 @@ module LeafOptics
 
 using ClimaCache: HyperspectralAbsorption, HyperspectralRadiation, HyperspectralLeafBiophysics, WaveLengthSet
 using ClimaCache: MonoMLGrassSPAC, MonoMLPalmSPAC, MonoMLTreeSPAC
-using ClimaCache: AVOGADRO, H_PLANCK, LIGHT_SPEED, M_H₂O, ρ_H₂O
+using EmeraldConstants: AVOGADRO, H_PLANCK, LIGHT_SPEED, M_H₂O, ρ_H₂O
 using SpecialFunctions: expint
 using UnPack: @unpack
 
