477f3a857acab5eaf0969dc78dea00d30ca61c9b
Yujie Wang <jesiner@gmail.com>
2022-10-18 09:30:21 -0700

    use EmeraldConstants

--- src/SoilHydraulics.jl
+++ src/SoilHydraulics.jl
@@ -1,8 +1,8 @@
 module SoilHydraulics
 
 using ClimaCache: MonoMLGrassSPAC, MonoMLPalmSPAC, MonoMLTreeSPAC, NonSteadyStateFlow, Root, SteadyStateFlow, VanGenuchten
-using ClimaCache: CP_L, CP_L_MOL, M_H₂O, Λ_THERMAL_H₂O, ρ_H₂O, ρg_MPa
 using ConstrainedRootSolvers: ReduceStepMethodND, SolutionToleranceND, find_peak
+using EmeraldConstants: CP_L, CP_L_MOL, M_H₂O, Λ_THERMAL_H₂O, ρ_H₂O, ρg_MPa
 using UnPack: @unpack
 using WaterPhysics: relative_surface_tension, relative_viscosity
 
