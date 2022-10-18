c96ab12c666ab8401e84372fd0dc9b4ac419b957
Yujie Wang <jesiner@gmail.com>
2022-10-18 00:42:20 -0700

    more systematic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,67 +3,83 @@ using Test
 using WaterPhysics
 
 
+@testset verbose = true "WaterPhysics" begin
+    @testset "Capillary Pressure" begin
+        for FT in [Float32, Float64]
+            rand_r = (rand(FT) + 20) * FT(1e-6);
+            rand_T = rand(FT) + 298;
+            rand_α = rand(FT) * 50;
+            for result in [ WaterPhysics.capillary_pressure(rand_r, rand_T),
+                            WaterPhysics.capillary_pressure(rand_r, rand_T, rand_α) ]
+                @test PkgUtility.FT_test(result, FT);
+                @test PkgUtility.NaN_test(result);
+            end;
+        end;
+    end;
 
+    @testset "Diffusive Coefficient" begin
+        for FT in [Float32, Float64]
+            rand_T  = rand(FT) + 298;
+            gas_air = WaterPhysics.TraceGasAir{FT}();
+            gas_CO₂ = WaterPhysics.TraceGasCO₂{FT}();
+            liq_H₂O = WaterPhysics.TraceLiquidH₂O{FT}();
+            for result in [ WaterPhysics.diffusive_coefficient(rand_T, gas_CO₂, gas_air),
+                            WaterPhysics.diffusive_coefficient(rand_T, gas_CO₂, liq_H₂O),
+                            WaterPhysics.relative_diffusive_coefficient(rand_T),
+                            WaterPhysics.relative_diffusive_coefficient(rand_T, gas_CO₂, liq_H₂O) ]
+                @test PkgUtility.FT_test(result, FT);
+                @test PkgUtility.NaN_test(result);
+            end;
+            @test WaterPhysics.relative_diffusive_coefficient(FT(298.15)) ≈ 1;
+        end;
+    end;
 
-@testset "WaterPhysics --- functions" begin
-    for FT in [Float32, Float64]
-        rand_r = (rand(FT) + 20) * FT(1e-6);
-        rand_T = rand(FT) + 298;
-        rand_α = rand(FT) * 50;
-        rand_Ψ = rand(FT) - 3;
+    @testset "Latent Heat of Vapor" begin
+        for FT in [Float32, Float64]
+            rand_T = rand(FT) + 298;
+            for result in [ WaterPhysics.latent_heat_vapor(rand_T) ]
+                @test PkgUtility.FT_test(result, FT);
+                @test PkgUtility.NaN_test(result);
+            end
+        end;
+    end;
 
-        gas_air = TraceGasAir{FT}();
-        gas_CO₂ = TraceGasCO₂{FT}();
-        gas_O₂  = TraceGasO₂{FT}();
-        liq_H₂O = TraceLiquidH₂O{FT}();
+    @testset "Surface Tension" begin
+        for FT in [Float32, Float64]
+            rand_T = rand(FT) + 298;
+            for result in [ WaterPhysics.surface_tension(rand_T),
+                            WaterPhysics.relative_surface_tension(rand_T) ]
+                @test PkgUtility.FT_test(result, FT);
+                @test PkgUtility.NaN_test(result);
+            end;
+            @test WaterPhysics.relative_surface_tension(FT(298.15)) ≈ 1;
+        end;
+    end;
 
-        # capillary_pressure
-        for result in [ capillary_pressure(rand_r, rand_T),
-                        capillary_pressure(rand_r, rand_T, rand_α) ]
-            @test FT_test(result, FT);
-            @test NaN_test(result);
-        end
+    @testset "Saturated Vapor Pressure" begin
+        for FT in [Float32, Float64]
+            rand_T = rand(FT) + 298;
+            rand_Ψ = rand(FT) - 3;
+            for result in [ WaterPhysics.pressure_correction(rand_T, rand_Ψ),
+                            WaterPhysics.saturation_vapor_pressure(rand_T),
+                            WaterPhysics.saturation_vapor_pressure(rand_T, rand_Ψ),
+                            WaterPhysics.saturation_vapor_pressure_slope(rand_T),
+                            WaterPhysics.saturation_vapor_pressure_slope(rand_T, rand_Ψ) ]
+                @test PkgUtility.FT_test(result, FT);
+                @test PkgUtility.NaN_test(result);
+            end;
+        end;
+    end;
 
-        # diffusive_coefficient
-        for result in [ diffusive_coefficient(rand_T, gas_CO₂, gas_air),
-                        diffusive_coefficient(rand_T, gas_CO₂, liq_H₂O),
-                        relative_diffusive_coefficient(rand_T),
-                        relative_diffusive_coefficient(rand_T, gas_CO₂, liq_H₂O) ]
-            @test FT_test(result, FT);
-            @test NaN_test(result);
-        end
-        @test relative_diffusive_coefficient(FT(298.15)) ≈ 1;
-
-        # latent_heat_vapor
-        for result in [ latent_heat_vapor(rand_T) ]
-            @test FT_test(result, FT);
-            @test NaN_test(result);
-        end
-
-        # surface_tension and relative_surface_tension
-        for result in [ surface_tension(rand_T),
-                        relative_surface_tension(rand_T) ]
-            @test FT_test(result, FT);
-            @test NaN_test(result);
-        end
-        @test relative_surface_tension(FT(298.15)) ≈ 1;
-
-        # saturation_vapor_pressure
-        for result in [ pressure_correction(rand_T, rand_Ψ),
-                        saturation_vapor_pressure(rand_T),
-                        saturation_vapor_pressure(rand_T, rand_Ψ),
-                        saturation_vapor_pressure_slope(rand_T),
-                        saturation_vapor_pressure_slope(rand_T, rand_Ψ) ]
-            @test FT_test(result, FT);
-            @test NaN_test(result);
-        end
-
-        # viscosity and relative_viscosity
-        for result in [ viscosity(rand_T),
-                        relative_viscosity(rand_T) ]
-            @test FT_test(result, FT);
-            @test NaN_test(result);
-        end
-        @test relative_viscosity(FT(298.15)) ≈ 1;
-    end
-end
+    @testset "Viscosity" begin
+        for FT in [Float32, Float64]
+            rand_T = rand(FT) + 298;
+            for result in [ WaterPhysics.viscosity(rand_T),
+                            WaterPhysics.relative_viscosity(rand_T) ]
+                @test PkgUtility.FT_test(result, FT);
+                @test PkgUtility.NaN_test(result);
+            end;
+            @test WaterPhysics.relative_viscosity(FT(298.15)) ≈ 1;
+        end;
+    end;
+end;
