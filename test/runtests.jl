c8b3f91411a3be04a5b61a11ec032f0aa139bc36
Yujie Wang <jesiner@gmail.com>
2022-10-31 18:52:40 -0700

    combine tests in Land for Github Actions

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,12 @@
 using Land
-using Pkg
 using Test
 
 
-@testset verbose = true "CliMA Land" begin
-    for pkgname in [
-                "EmeraldConstants",
-                "WaterPhysics",
-                "ClimaCache",
-                "LeafOptics",
-                "CanopyRadiativeTransfer",
-                "Photosynthesis",
-                "SoilHydraulics",
-                "PlantHydraulics",
-                "StomataModels",
-                "SoilPlantAirContinuum"]
-        Pkg.test(pkgname);
-        @test true;
+pkgs = ["EmeraldConstants", "WaterPhysics", "ClimaCache", "LeafOptics", "CanopyRadiativeTransfer", "Photosynthesis", "SoilHydraulics", "PlantHydraulics", "StomataModels", "SoilPlantAirContinuum"];
+
+@testset verbose = true "CliMA Land Modules" begin
+    for pkg in pkgs
+        @info "Testing package $(pkg).jl...";
+        include("../packages/$(pkg).jl/test/runtests.jl");
     end;
 end;
