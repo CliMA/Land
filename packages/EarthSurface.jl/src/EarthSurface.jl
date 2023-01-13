016502f478907de7daa71a4eba9271ed3659042f
Yujie Wang <jesiner@gmail.com>
2023-01-13 09:26:57 -0800

    remove the module files temporarily

--- packages/EarthSurface.jl/src/EarthSurface.jl
+++
@@ -1,9 +0,0 @@
-module EarthSurface
-
-#using EmeraldConstants: YEAR_D
-
-
-include("solar_zenith.jl")
-
-
-end # module
--- packages/EmeraldConstants.jl/src/EmeraldConstants.jl
+++
@@ -1,9 +0,0 @@
-module EmeraldConstants
-
-using DocStringExtensions: TYPEDEF, TYPEDFIELDS
-
-
-include("universal.jl")
-
-
-end # module
